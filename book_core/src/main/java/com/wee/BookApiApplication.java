d3d7cc209dea8d096ac17d2c312bcc80044d9967
yuzhang <1580074674@qq.com>
2016-05-21 13:22:32 +0800

    [BookAPI][Yu] Add logback

--- book_core/src/main/java/com/wee/BookApiApplication.java
+++ book_core/src/main/java/com/wee/BookApiApplication.java
@@ -1,7 +1,10 @@
 package com.wee;
 
+import com.wee.filter.RequestLoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(value = "classpath:common.properties")
@@ -10,5 +13,12 @@ public class BookApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApiApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean requestLoggingFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new RequestLoggingFilter());
+        return registrationBean;
     }
 }
---
+++ book_core/src/main/java/com/wee/filter/RequestLoggingFilter.java
@@ -0,0 +1,37 @@
+package com.wee.filter;
+
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+import static java.lang.String.format;
+
+public class RequestLoggingFilter implements Filter {
+
+    private final Logger logger = Logger.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        if (request.getQueryString() == null) {
+            logger.info(format("%s,%s", request.getMethod(), request.getRequestURL()));
+        } else {
+            logger.info(format("%s,%s?%s", request.getMethod(), request.getRequestURL(), request.getQueryString()));
+        }
+        chain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
